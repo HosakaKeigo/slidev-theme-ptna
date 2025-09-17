6e1554a4725267aa09f82fb77a7d3746019cd026
HosakaKeigo <gomagoma313@gmail.com>
2025-09-18 01:08:55 +0900

    feat: PageIndicatorコンポーネントの進捗表示をSVGで実装

--- components/PageIndicator.vue
+++ components/PageIndicator.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
-// No props needed - uses $slidev global
+import { computed } from 'vue';
+
+const r = 40;
+const strokeWidth = 13;
+const circumference = 2 * Math.PI * r
+
+const progress = computed(() => {
+  return ($slidev.nav.currentPage / $slidev.nav.total) * circumference;
+});
+
+const offset = computed(() => {
+  return circumference - progress.value;
+});
 </script>
 
 <template>
-  <div class="absolute bottom-4 right-4 text-sm text-gray-500">
-    <div class="flex items-center">
-      <div class="w-2 h-2 bg-ptna-primary rounded-full mr-2"></div>
-      <span class="font-mono"
-        >{{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}</span
-      >
-    </div>
-  </div>
+  <footer class="abs-br m-3">
+    <svg w-24 viewBox="0 0 100 100">
+      <circle class="base-circle" cx="50" cy="50" :r="r" fill="none" :stroke-width="strokeWidth" />
+      <circle class="progress-circle" ref="progressCircle" cx="50" cy="50" :r="r" transform="rotate(-90 50 50)" fill="none" :stroke-width="strokeWidth" :stroke-dasharray="circumference" :stroke-dashoffset="offset"/>
+      <text class="progress-text" font-size="8" x="50" y="53" dominant-baseline="middle" text-anchor="middle">{{ $slidev.nav.currentPage }}</text>
+    </svg>
+  </footer>
 </template>
 
-<style scoped>
-.bg-ptna-primary {
-  background-color: var(--ptna-primary);
+<style>
+:root {
+  --bg-stroke-color: #e2e8f0;
+  --progress-stroke-color: var(--ptna-primary);
+  --text-fill-color: #020617;
+}
+
+.base-circle {
+  stroke: var(--bg-stroke-color);
+}
+
+.progress-circle {
+  stroke: var(--progress-stroke-color);
+}
+
+.progress-text {
+  fill: var(--text-fill-color);
 }
 </style>
